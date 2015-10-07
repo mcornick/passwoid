4fac6fc1181c7cd81d3c098aeaad919a74cbe6ff
Mark Cornick <mark@markcornick.com>
2015-10-07 15:11:52 -0400

    Use eslint directly

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 var path = require('path')
 var gulp = require('gulp')
-var standard = require('gulp-standard')
+var eslint = require('gulp-eslint')
 var excludeGitignore = require('gulp-exclude-gitignore')
 var mocha = require('gulp-mocha')
 var istanbul = require('gulp-istanbul')
@@ -13,8 +13,9 @@ var coveralls = require('gulp-coveralls')
 gulp.task('static', function () {
   return gulp.src('**/*.js')
     .pipe(excludeGitignore())
-    .pipe(standard())
-    .pipe(standard.reporter('default', {breakOnError: true}))
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
 })
 
 gulp.task('nsp', function (cb) {
