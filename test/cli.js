136318be97cba294814e102d273033dce437a32e
Mark Cornick <mark@markcornick.com>
2015-11-09 17:08:57 -0500

    DRY up stdout setup in tests

--- test/cli.js
+++ test/cli.js
@@ -18,35 +18,33 @@ var cli = require('../lib/cli');
 var test = require('ava');
 var stream = require('mock-utf8-stream');
 
+test.beforeEach('set up stdout', function (t) {
+  t.context.stdout = new stream.MockWritableStream();
+  t.context.stdout.startCapture();
+  t.end();
+});
+
 test('given no length, creates a password of default length', function (t) {
   t.plan(1);
-  var stdout = new stream.MockWritableStream();
-  stdout.startCapture();
-  cli({argv: ['node', 'bin.js'], stdout: stdout});
-  t.is(stdout.capturedData.trim().length, 16);
+  cli({argv: ['node', 'bin.js'], stdout: t.context.stdout});
+  t.is(t.context.stdout.capturedData.trim().length, 16);
 });
 
 test('given a specific length, creates a password of that length', function (t) {
   t.plan(1);
-  var stdout = new stream.MockWritableStream();
-  stdout.startCapture();
-  cli({argv: ['node', 'bin.js', 8], stdout: stdout});
-  t.is(stdout.capturedData.trim().length, 8);
+  cli({argv: ['node', 'bin.js', 8], stdout: t.context.stdout});
+  t.is(t.context.stdout.capturedData.trim().length, 8);
 });
 
 test('given a bogus length, creates a password of default length', function (t) {
   t.plan(1);
-  var stdout = new stream.MockWritableStream();
-  stdout.startCapture();
-  cli({argv: ['node', 'bin.js', 'pants'], stdout: stdout});
-  t.is(stdout.capturedData.trim().length, 16);
+  cli({argv: ['node', 'bin.js', 'pants'], stdout: t.context.stdout});
+  t.is(t.context.stdout.capturedData.trim().length, 16);
 });
 
 test('given a too-short length, throws an error', function (t) {
   t.plan(1);
-  var stdout = new stream.MockWritableStream();
-  t.throws(
-    function () {
-      cli({argv: ['node', 'bin.js', 1], stdout: stdout});
-    }, 'Cannot generate password of length 1');
+  t.throws(function () {
+    cli({argv: ['node', 'bin.js', 1], stdout: t.context.stdout});
+  }, 'Cannot generate password of length 1');
 });
